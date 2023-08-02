b156ce83f385d243f116e5998db46291da6d2eb6
ANT <bit0316@naver.com>
2023-08-02 22:27:39 +0900

    Feat: [C] Linear List

---
+++ DataStructure/Homework01_01/listS.c
@@ -0,0 +1,47 @@
+﻿/* listS.c */
+#include "listS.h"
+
+int insertElement(int list[], int size, int element) {
+	int position, target, move = 0;
+
+	for (position = 0; position < size; position++) {
+		if (list[position] >= element) {
+			target = position;
+			break;
+		}
+	}
+
+	if (position == size) {
+		target = position;
+	}
+
+	for (position = size; position > target; position--) {
+		list[position] = list[position - 1];
+		move++;
+	}
+	list[target] = element;
+
+	return move;
+}
+
+int deleteElement(int list[], int size, int element) {
+	int position, target = -1, move = 0;
+
+	for (position = 0; position < size; position++) {
+		if (list[position] == element) {
+			target = position;
+			break;
+		}
+	}
+
+	if (position == size) {
+		return size;
+	}
+
+	for (position = target; position < size - 1; position++) {
+		list[position] = list[position + 1];
+		move++;
+	}
+
+	return move;
+}
---
+++ DataStructure/Homework01_01/listS.h
@@ -0,0 +1,6 @@
+﻿/* listS.h */
+#pragma once
+#define MAX	10
+
+int insertElement(int list[], int size, int element);
+int deleteElement(int list[], int size, int element);
