f2ff4378398485f60d1f2a676528a7e48c748e5b
ANT <bit0316@naver.com>
2023-08-02 22:27:49 +0900

    Feat: [C] Polynomial

---
+++ DataStructure/Homework01_02/addPoly.c
@@ -0,0 +1,26 @@
+﻿/* addPoly.c */
+#include "addPoly.h"
+
+polynomial addPoly(polynomial A, polynomial B) {
+	polynomial P, bigP, smallP;
+	int diff_degree;
+
+	P.degree = MAX(A.degree, B.degree);
+	(A.degree > B.degree) ? (bigP = A, smallP = B) : (bigP = B, smallP = A);
+	diff_degree = bigP.degree - smallP.degree;
+
+	for (int i = 0; i <= P.degree; i++)
+		P.coef[i] = (i < diff_degree) ? (bigP.coef[i]) : (bigP.coef[i] + smallP.coef[i - diff_degree]);
+
+	return P;
+}
+
+void printPoly(polynomial P) {
+	int exp = P.degree;
+
+	for (int i = 0; i <= P.degree; i++) {
+		printf("%.fx^%d", P.coef[i], exp--);
+		if (i < P.degree)
+			printf(" + ");
+	}
+}
---
+++ DataStructure/Homework01_02/main.c
@@ -0,0 +1,22 @@
+﻿/*
+* File name : main.c
+* Author : Kim Yeong Ho
+* Date : August 02, 2023
+* Major feature : Polynomial
+*/
+#include <stdio.h>
+#include "addPoly.h"
+
+int main(void) {
+	polynomial A = { 3, { 4, 3, 5, 0 } };
+	polynomial B = { 4, { 3, 1, 0, 2, 1 } };
+	polynomial C;
+
+	C = addPoly(A, B);
+
+	printf("\n A(x) = ");	printPoly(A);
+	printf("\n B(x) = ");	printPoly(B);
+	printf("\n C(x) = ");	printPoly(C);
+
+	return 0;
+}
