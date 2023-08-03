6c1526eddf0b08b83b5e5c755ce2cbba004a2401
ANT <bit0316@naver.com>
2023-08-03 18:03:00 +0900

    Feat: [C] Singly Linked List

---
+++ DataStructure/Homework02_01/insertLinkedList.c
@@ -0,0 +1,81 @@
+﻿/* insertLinkedList.c */
+#include "insertLinkedList.h"
+
+linkedList_h* createLinkedList_h(void) {
+	linkedList_h* L;
+
+	L = (linkedList_h*)malloc(sizeof(linkedList_h));
+	L->head = NULL;
+
+	return L;
+}
+
+void freeLinkedList_h(linkedList_h* L) {
+	listNode* tr;
+
+	tr = L->head;
+	
+	while (tr != NULL) {
+		L->head = tr->link;
+		free(tr);
+		tr = L->head;
+	}
+}
+
+void printList(linkedList_h* L) {
+	listNode* tr;
+
+	tr = L->head;
+	
+	while (tr != NULL) {
+		printf("%s ", tr->data);
+		tr = tr->link;
+	}
+	printf("\n\n");
+}
+
+void insertFirstNode(linkedList_h* L, char* x) {
+	listNode* newNode;
+
+	newNode = (listNode*)malloc(sizeof(listNode));
+	strcpy(newNode->data, x);
+	newNode->link = L->head;
+	
+	L->head = newNode;
+}
+
+void insertMiddleNode(linkedList_h* L, listNode* pre, char* x) {
+	listNode* newNode;
+
+	newNode = (listNode*)malloc(sizeof(listNode));
+	strcpy(newNode->data, x);
+
+	if (L->head == NULL) {
+		L->head = newNode;
+		newNode->link = NULL;
+	}
+	else {
+		newNode->link = pre->link;
+		pre->link = newNode;
+	}
+}
+
+void insertLastNode(linkedList_h* L, char* x) {
+	listNode* newNode;
+	listNode* tr;
+
+	newNode = (listNode*)malloc(sizeof(listNode));
+	strcpy(newNode->data, x);
+	newNode->link = NULL;
+	
+	tr = L->head;
+
+	if (tr == NULL) {
+		tr = newNode;
+		return;
+	}
+
+	while (tr->link != NULL)
+		tr = tr->link;
+	tr->link = newNode;
+}
---
+++ DataStructure/Homework02_01/main.c
@@ -0,0 +1,33 @@
+﻿/*
+* File name : main.c
+* Author : Kim Yeong Ho
+* Date : August 03, 2023
+* Major feature : Singly Linked List
+*/
+#include <stdio.h>
+#include "InsertLinkedList.h"
+
+int main(void) {
+	linkedList_h* L;
+	L = createLinkedList_h();
+	printf("(1) 공백 리스트 생성하기!\n");
+	printList(L);
+	
+	printf("(2) 리스트에 [수] 노드 삽입하기!\n");
+	insertFirstNode(L, "수");
+	printList(L);
+
+	printf("(3) 리스트에 마지막에 [금] 노드 삽입하기!\n");
+	insertLastNode(L, "금");
+	printList(L);
+
+	printf("(4) 리스트에 첫 번째에 [월] 노드 삽입하기!\n");
+	insertFirstNode(L, "월");
+	printList(L);
+
+	printf("(5) 리스트에 공간을 해제하여 공백 리스트로 만들기!\n");
+	freeLinkedList_h(L);
+	printList(L);
+	
+	return 0;
+}
