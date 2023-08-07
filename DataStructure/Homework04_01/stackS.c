25330d6c167403a89b372215884e49dd81470112
ANT <bit0316@naver.com>
2023-08-07 16:08:02 +0900

    Feat: [C] Sequential Stack Operation

---
+++ DataStructure/Homework04_01/stackS.c
@@ -0,0 +1,56 @@
+﻿/* stackS.c */
+#include "stackS.h"
+
+int top = -1;
+
+int isStackEmpty() {
+	if (top < 0) {
+		return TRUE;
+	}
+	else {
+		return FALSE;
+	}
+}
+
+int isStackFull() {
+	if ((top + 1) >= STACK_SIZE) {
+		return TRUE;
+	}
+	else {
+		return FALSE;
+	}
+}
+
+void push(element item) {
+	if (!isStackFull()) {
+		stack[++top] = item;
+	}
+	else {
+		return;
+	}
+}
+
+element pop() {
+	if (!isStackEmpty()) {
+		return stack[top--];
+	}
+	else {
+		return 0;
+	}
+}
+
+element peek() {
+	if (!isStackEmpty()) {
+		return stack[top];
+	}
+	else {
+		return 0;
+	}
+}
+
+void printStack() {
+	printf("\n스택 출력 : ");
+	for (int i = 0; i <= top; ++i) {
+		printf("%d ", stack[i]);
+	}
+}
---
+++ DataStructure/Homework04_01/stackS.h
@@ -0,0 +1,15 @@
+﻿/* stackS.h */
+#pragma once
+#define STACK_SIZE	100
+#define FALSE		0
+#define TRUE		!FALSE
+
+typedef int element;
+element stack[STACK_SIZE];
+
+int isStackEmpty();
+int isStackFull();
+void push(element item);
+element pop();
+element peek();
+void printStack();
